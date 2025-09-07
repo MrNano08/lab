8307f2bf2ec557af425bac78049f58104e17869a
Ronaldo Mora Díaz <Imrnano08@gmail.com>
2025-09-06 19:44:03 -0600

    1st try to repair whithout cursor ia

--- risk-calculator-backend/dist/risk/dto/config.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ConfigResponseDto = void 0;
-class ConfigResponseDto {
-}
-exports.ConfigResponseDto = ConfigResponseDto;
-//# sourceMappingURL=config.dto.js.map
