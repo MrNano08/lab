07ede57fc13f4aa1cc9af2a2ca6675b2ac227fac
Ronaldo Mora Díaz <Imrnano08@gmail.com>
2025-09-08 19:56:23 -0600

    Se cambió el proyecto a nextjs

--- risk-calculator-backend/dist/risk/dto/calc.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CalcRequestDto {
-    likelihood: number;
-    impact: number;
-}
-export declare class CalcResponseDto {
-    score: number;
-    category: 'Low' | 'Medium' | 'High' | 'Extreme';
-}
--- risk-calculator-backend/dist/risk/risk.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { CalcRequestDto, CalcResponseDto } from './dto/calc.dto';
-import { ConfigResponseDto } from './dto/config.dto';
-export declare class RiskService {
-    private readonly defaultConfig;
-    getConfig(): ConfigResponseDto;
-    calculateRisk(request: CalcRequestDto): CalcResponseDto;
-}
